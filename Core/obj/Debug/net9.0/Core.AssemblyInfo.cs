a822817fadfe55ecbe3888d80e00b33b8acc1168
mongmycha <mongmycha01@gmail.com>
2025-09-27 19:38:05 +0600

    End of section 4

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f800a359aa56fdaac3170c819baab9d3923b4199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f267a6dc985c47f6bd6dd9d0d0f5577abd03ff3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
